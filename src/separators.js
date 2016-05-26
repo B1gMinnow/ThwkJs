be55eab3e527c935b09f3633ab327b2d2bcd0a5f
BigMinnow <b1gminnow@outlook.com>
2016-05-26 20:44:57 +0800

    anther try

--- src/separators.js
+++ src/separators.js
@@ -3,21 +3,22 @@ function addComma(s)
 {
   var re1 = /\d{1,3}/;
   var re2 = /\d{4,6}/;
-  var re3 = /\d{7,}/;
+  var re3 = /\d{7,9}/;
   if(re1.test(s))
     {
       return s;
     }
 
+
     if(re2.test(s))
     {
       
-      return substring(0,s.length - 3) + ',' + substring(s.length-3)
+      return substring(0,s.length - 3) + ',' + substring(s.length-3);
       
     }
     if(re3.test(s))
-      return substring(0,s.length-6) + ',' + substring(s.length - 6, s.length - 3 ) + ',' + substring(s.length - 3);
-    
+      return s.substring(0,s.length-6) + ',' + s.substring(s.length - 6, s.length - 3 ) + ',' + s.substring(s.length - 3);
+
     return '';
 }
 
